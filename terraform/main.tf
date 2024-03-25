1dc07d9073d66c83fedf6574c39555cf6f409cd6
AllenShap <164272261+AllenShap@users.noreply.github.com>
2024-03-25 15:57:37 -0700

    Update main.tf
    Removed variables and backend block to try another approach to connecting to the backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,3 @@
-variable "AZURE_ACCESS_KEY" {
-  type      = string
-  sensitive = true
-}
-variable "AZURE_STORAGE_ACCOUNT_NAME" {
-  type      = string
-  sensitive = true
-}
-variable "AZURE_CONTAINER_NAME" {
-  type      = string
-  sensitive = true
-}
-
-
 terraform {
   required_providers {
     azurerm = {
@@ -19,15 +5,8 @@ terraform {
       version = "~>3.94.0"
     }
   }
-  backend "azurerm" {
-    access_key = var.AZURE_ACCESS_KEY
-    storage_account_name = var.AZURE_STORAGE_ACCOUNT_NAME
-    container_name = var.AZURE_CONTAINER_NAME
-    key = "terraform.tfstate"
-}
   required_version = ">=1.1.0"
 }
-
 provider "azurerm" {
   features {
     resource_group {
