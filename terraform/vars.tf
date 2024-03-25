a52a3f8909f2db10782867f646bcc32bc878087e
AllenShap <164272261+AllenShap@users.noreply.github.com>
2024-03-25 16:10:33 -0700

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,12 @@
+variable "AZURE_ACCESS_KEY" {
+  type      = string
+  sensitive = true
+}
+variable "AZURE_STORAGE_ACCOUNT_NAME" {
+  type      = string
+  sensitive = true
+}
+variable "AZURE_CONTAINER_NAME" {
+  type      = string
+  sensitive = true
+}
