71f2637ab46abd579071b2b7136f65c46c800c12
AllenShap <164272261+AllenShap@users.noreply.github.com>
2024-04-30 17:10:49 -0700

    Update main.tf
    Changed SKU of TextAnalytics to Free to save on costs.
    Added the creation of a new resource; "azurerm_search_service".
    
    Added new app_settings field in resource: "azurerm_linux_function_app" to support new AI implementation.
    	Explicity set the "app_scale_limit" to "1" to prevent concurrency and scaling out of the function app

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,14 +63,28 @@ resource "azurerm_cognitive_account" "CA-USNYTPythonTwitterBot" {
   location            = azurerm_resource_group.RG-USNYTPythonTwitterBot.location
   resource_group_name = azurerm_resource_group.RG-USNYTPythonTwitterBot.name
   kind                = "TextAnalytics"
-  sku_name            = "S"
+  sku_name            = "F0"
+
+  custom_question_answering_search_service_id  = azurerm_search_service.Search-USNYTPythonTwitterBot.id
+  custom_question_answering_search_service_key = azurerm_search_service.Search-USNYTPythonTwitterBot.primary_key
+
+
+  depends_on = [azurerm_resource_group.RG-USNYTPythonTwitterBot]
+}
+
+resource "azurerm_search_service" "Search-USNYTPythonTwitterBot" {
+  name                = "freenytimesaisearch"
+  location            = azurerm_resource_group.RG-USNYTPythonTwitterBot.location
+  resource_group_name = azurerm_resource_group.RG-USNYTPythonTwitterBot.name
+  sku                 = "free"
 
   depends_on = [azurerm_resource_group.RG-USNYTPythonTwitterBot]
 }
 
 
+
 resource "azurerm_cosmosdb_account" "USNYTPythonTwitterBotCDBAccount" {
-  depends_on  = [azurerm_storage_account.USNYTPythonTwitterBotSA]
+  depends_on          = [azurerm_storage_account.USNYTPythonTwitterBotSA]
   name                = "usnytpythontwitterbotcdb"
   location            = azurerm_resource_group.RG-USNYTPythonTwitterBot.location
   resource_group_name = azurerm_resource_group.RG-USNYTPythonTwitterBot.name
@@ -95,14 +109,14 @@ resource "azurerm_cosmosdb_account" "USNYTPythonTwitterBotCDBAccount" {
 }
 
 resource "azurerm_cosmosdb_sql_database" "USNYTPythonTwitterBotCDB" {
-  depends_on  = [azurerm_storage_account.USNYTPythonTwitterBotSA]
+  depends_on          = [azurerm_storage_account.USNYTPythonTwitterBotSA]
   name                = "USNYTPythonTwitterBot"
   resource_group_name = azurerm_resource_group.RG-USNYTPythonTwitterBot.name
   account_name        = azurerm_cosmosdb_account.USNYTPythonTwitterBotCDBAccount.name
 }
 
 resource "azurerm_cosmosdb_sql_container" "USNYTPythonTwitterBotCDBContainer" {
-  depends_on  = [azurerm_storage_account.USNYTPythonTwitterBotSA]
+  depends_on          = [azurerm_storage_account.USNYTPythonTwitterBotSA]
   name                = "USNYTPythonTwitterBotContainer"
   resource_group_name = azurerm_resource_group.RG-USNYTPythonTwitterBot.name
 
@@ -149,34 +163,37 @@ resource "azurerm_application_insights" "AppInsights-USNYTPythonTwitterBot" {
 
 
 resource "azurerm_linux_function_app" "USNYTPythonTwitterBotFA-PROD" {
-  depends_on  = [azurerm_storage_account.USNYTPythonTwitterBotSA]
+  depends_on          = [azurerm_storage_account.USNYTPythonTwitterBotSA]
   name                = "USNYTPythonTwitterBotter"
   resource_group_name = azurerm_resource_group.RG-USNYTPythonTwitterBot.name
   location            = azurerm_resource_group.RG-USNYTPythonTwitterBot.location
 
-  storage_account_name       = azurerm_storage_account.USNYTPythonTwitterBotSA.name
-  storage_account_access_key = azurerm_storage_account.USNYTPythonTwitterBotSA.primary_access_key
-  service_plan_id            = azurerm_service_plan.ASP-USNYTPythonTwitterBot-617e.id
+  storage_account_name         = azurerm_storage_account.USNYTPythonTwitterBotSA.name
+  storage_account_access_key   = azurerm_storage_account.USNYTPythonTwitterBotSA.primary_access_key
+  service_plan_id              = azurerm_service_plan.ASP-USNYTPythonTwitterBot-617e.id
   content_share_force_disabled = true
 
   app_settings = {
-  COGNITIVE_ENDPOINT = azurerm_cognitive_account.CA-USNYTPythonTwitterBot.endpoint,
-  COGNITIVE_KEY = azurerm_cognitive_account.CA-USNYTPythonTwitterBot.primary_access_key,
-  COSMOS_DB_ENDPOINT = azurerm_cosmosdb_account.USNYTPythonTwitterBotCDBAccount.endpoint,
-  COSMOS_DB_NAME = azurerm_cosmosdb_sql_database.USNYTPythonTwitterBotCDB.name,
-  COSMOS_DB_CONTAINER_NAME = azurerm_cosmosdb_sql_container.USNYTPythonTwitterBotCDBContainer.name,
-  COSMOS_DB_CREDENTIAL = azurerm_cosmosdb_account.USNYTPythonTwitterBotCDBAccount.primary_key,
-  TWITTER_CONSUMER_KEY =  var.TWITTER_CONSUMER_KEY,
-  TWITTER_CONSUMER_SECRET =  var.TWITTER_CONSUMER_SECRET,
-  TWITTER_ACCESS_TOKEN =  var.TWITTER_ACCESS_TOKEN,
-  TWITTER_ACCESS_TOKEN_SECRET =  var.TWITTER_ACCESS_TOKEN_SECRET,
+    COGNITIVE_ENDPOINT        = azurerm_cognitive_account.CA-USNYTPythonTwitterBot.endpoint,
+    COGNITIVE_KEY             = azurerm_cognitive_account.CA-USNYTPythonTwitterBot.primary_access_key,
+    COGNITIVE_SEARCH_ENDPOINT = "https://${azurerm_search_service.Search-USNYTPythonTwitterBot.name}.search.windows.net"
+    COGNITIVE_SEARCH_KEY      = azurerm_search_service.Search-USNYTPythonTwitterBot.primary_key,
+    COSMOS_DB_ENDPOINT          = azurerm_cosmosdb_account.USNYTPythonTwitterBotCDBAccount.endpoint,
+    COSMOS_DB_NAME              = azurerm_cosmosdb_sql_database.USNYTPythonTwitterBotCDB.name,
+    COSMOS_DB_CONTAINER_NAME    = azurerm_cosmosdb_sql_container.USNYTPythonTwitterBotCDBContainer.name,
+    COSMOS_DB_CREDENTIAL        = azurerm_cosmosdb_account.USNYTPythonTwitterBotCDBAccount.primary_key,
+    TWITTER_CONSUMER_KEY        = var.TWITTER_CONSUMER_KEY,
+    TWITTER_CONSUMER_SECRET     = var.TWITTER_CONSUMER_SECRET,
+    TWITTER_ACCESS_TOKEN        = var.TWITTER_ACCESS_TOKEN,
+    TWITTER_ACCESS_TOKEN_SECRET = var.TWITTER_ACCESS_TOKEN_SECRET,
   }
 
   sticky_settings {
-     app_setting_names = ["COGNITIVE_ENDPOINT","COGNITIVE_KEY","TWITTER_CONSUMER_KEY","TWITTER_CONSUMER_SECRET","TWITTER_ACCESS_TOKEN","TWITTER_ACCESS_TOKEN_SECRET","COSMOS_DB_ENDPOINT","COSMOS_DB_NAME","COSMOS_DB_CONTAINER_NAME","COSMOS_DB_CREDENTIAL","FUNCTIONS_EXTENSION_VERSION","AzureWebJobsStorage","APPLICATIONINSIGHTS_CONNECTION_STRING","APPINSIGHTS_INSTRUMENTATIONKEY","FUNCTIONS_WORKER_RUNTIME","AzureWebJobsDashboard"]
+    app_setting_names = ["TWITTER_CONSUMER_KEY", "TWITTER_CONSUMER_SECRET", "TWITTER_ACCESS_TOKEN", "TWITTER_ACCESS_TOKEN_SECRET", "COSMOS_DB_ENDPOINT", "COSMOS_DB_NAME", "COSMOS_DB_CONTAINER_NAME", "COSMOS_DB_CREDENTIAL", "FUNCTIONS_EXTENSION_VERSION", "AzureWebJobsStorage", "APPLICATIONINSIGHTS_CONNECTION_STRING", "APPINSIGHTS_INSTRUMENTATIONKEY", "FUNCTIONS_WORKER_RUNTIME", "AzureWebJobsDashboard", "COGNITIVE_ENDPOINT", "COGNITIVE_KEY", "COGNITIVE_SEARCH_ENDPOINT", "COGNITIVE_SEARCH_KEY"]
   }
 
   site_config {
+    app_scale_limit                        = "1"
     application_insights_connection_string = azurerm_application_insights.AppInsights-USNYTPythonTwitterBot.connection_string
     application_insights_key               = azurerm_application_insights.AppInsights-USNYTPythonTwitterBot.instrumentation_key
     cors {
@@ -185,32 +202,35 @@ resource "azurerm_linux_function_app" "USNYTPythonTwitterBotFA-PROD" {
     application_stack {
       python_version = 3.11
     }
-}
+  }
 }
 
 
 resource "azurerm_linux_function_app_slot" "USNYTPythonTwitterBotFA-DEV" {
-  depends_on  = [azurerm_storage_account.USNYTPythonTwitterBotSA]
-  name                 = "dev"
-  function_app_id      = azurerm_linux_function_app.USNYTPythonTwitterBotFA-PROD.id
-  storage_account_name       = azurerm_storage_account.USNYTPythonTwitterBotSA.name
-  storage_account_access_key = azurerm_storage_account.USNYTPythonTwitterBotSA.primary_access_key
+  depends_on                   = [azurerm_storage_account.USNYTPythonTwitterBotSA]
+  name                         = "dev"
+  function_app_id              = azurerm_linux_function_app.USNYTPythonTwitterBotFA-PROD.id
+  storage_account_name         = azurerm_storage_account.USNYTPythonTwitterBotSA.name
+  storage_account_access_key   = azurerm_storage_account.USNYTPythonTwitterBotSA.primary_access_key
   content_share_force_disabled = true
 
   app_settings = {
-  COGNITIVE_ENDPOINT = azurerm_cognitive_account.CA-USNYTPythonTwitterBot.endpoint,
-  COGNITIVE_KEY = azurerm_cognitive_account.CA-USNYTPythonTwitterBot.primary_access_key,
-  COSMOS_DB_ENDPOINT = azurerm_cosmosdb_account.USNYTPythonTwitterBotCDBAccount.endpoint,
-  COSMOS_DB_NAME = azurerm_cosmosdb_sql_database.USNYTPythonTwitterBotCDB.name,
-  COSMOS_DB_CONTAINER_NAME = azurerm_cosmosdb_sql_container.USNYTPythonTwitterBotCDBContainer.name,
-  COSMOS_DB_CREDENTIAL = azurerm_cosmosdb_account.USNYTPythonTwitterBotCDBAccount.primary_key,
-  TWITTER_CONSUMER_KEY = var.TWITTER_CONSUMER_KEY,
-  TWITTER_CONSUMER_SECRET =  var.TWITTER_CONSUMER_SECRET,
-  TWITTER_ACCESS_TOKEN =  var.TWITTER_ACCESS_TOKEN,
-  TWITTER_ACCESS_TOKEN_SECRET =  var.TWITTER_ACCESS_TOKEN_SECRET,
+    COGNITIVE_ENDPOINT        = azurerm_cognitive_account.CA-USNYTPythonTwitterBot.endpoint,
+    COGNITIVE_KEY             = azurerm_cognitive_account.CA-USNYTPythonTwitterBot.primary_access_key,
+    COGNITIVE_SEARCH_ENDPOINT = "https://${azurerm_search_service.Search-USNYTPythonTwitterBot.name}.search.windows.net"
+    COGNITIVE_SEARCH_KEY      = azurerm_search_service.Search-USNYTPythonTwitterBot.primary_key,
+    COSMOS_DB_ENDPOINT          = azurerm_cosmosdb_account.USNYTPythonTwitterBotCDBAccount.endpoint,
+    COSMOS_DB_NAME              = azurerm_cosmosdb_sql_database.USNYTPythonTwitterBotCDB.name,
+    COSMOS_DB_CONTAINER_NAME    = azurerm_cosmosdb_sql_container.USNYTPythonTwitterBotCDBContainer.name,
+    COSMOS_DB_CREDENTIAL        = azurerm_cosmosdb_account.USNYTPythonTwitterBotCDBAccount.primary_key,
+    TWITTER_CONSUMER_KEY        = var.TWITTER_CONSUMER_KEY,
+    TWITTER_CONSUMER_SECRET     = var.TWITTER_CONSUMER_SECRET,
+    TWITTER_ACCESS_TOKEN        = var.TWITTER_ACCESS_TOKEN,
+    TWITTER_ACCESS_TOKEN_SECRET = var.TWITTER_ACCESS_TOKEN_SECRET,
   }
 
   site_config {
+    app_scale_limit                        = "1"
     application_insights_connection_string = azurerm_application_insights.AppInsights-USNYTPythonTwitterBot.connection_string
     application_insights_key               = azurerm_application_insights.AppInsights-USNYTPythonTwitterBot.instrumentation_key
     cors {
@@ -219,5 +239,5 @@ resource "azurerm_linux_function_app_slot" "USNYTPythonTwitterBotFA-DEV" {
     application_stack {
       python_version = 3.11
     }
-}
+  }
 }
